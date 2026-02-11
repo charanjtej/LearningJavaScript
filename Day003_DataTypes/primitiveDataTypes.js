d3b560cc397b332c5608af8dea9dfa1bfedd2e2f
jetty charan tej <charantejj@gmail.com>
2026-02-11 21:38:51 +0530

    Explanation of Primitive Data Types

---
+++ Day003_DataTypes/primitiveDataTypes.js
@@ -0,0 +1,17 @@
+//primitive data type.
+let name = "Ram";
+let age = 25; 
+//String is always enclosed in quotes (') or (")
+//JavaScript is dynamic programing language it automatically based on value understands type of variable.
+let mobile = '8881118881'; //it is automatically becomes a String.
+let isPresent = true; //true or false without quotes are not strings, they are known as booleans.
+let isMarried; //I'm not assigning any value to it, "undefined"
+
+//to print anything on terminal we use the statement as console.log()
+//to identify the type of a variable we use an operator in JS: typeof
+console.log(typeof age); //number
+let myHouseAddress = null;
+
+console.log(typeof myHouseAddress); // what must be ideally output of it. => object
+
+//symbol helps us to create immutable key for object
